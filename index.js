fac614cb6d24fce8753999f73035835084cba69b
urgyan <urgyanue195125ece@gmail.com>
2021-11-13 19:24:26 +0530

    require espress-session ,passport,local mongostore

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const port = 8001;
 // express layout
 const expressLayouts = require('express-ejs-layouts');
 const db=require('./config/mongoose');
+// used for session cookies
+
+const session = require('express-session');
+
+const passport = require('passport');
+
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
 
 app.use(express.urlencoded());
 
@@ -17,11 +25,41 @@ app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-// use express  router
-app.use('/', require('./routes'))
+
 // set up the views engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+// mongo store is used to store the session cookie in the db
+app.use(session({
+    name:'codeial',
+    // TODO change the secret before deyployment in production mode
+    secret: 'blhasomething',
+    saveUninitialized: false,
+    resave: false,
+
+    cookie: {
+        maxAge:(1000 * 60 * 100)
+    },
+    store: MongoStore.create(
+        {
+          mongoUrl: 'mongodb://localhost/codeial_development',
+          autoRemove: "disabled",
+        },
+        function(err){
+            console.log(err || 'connect-mongodb setup ok');
+        }
+    )
+
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+// use express  router
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
 
