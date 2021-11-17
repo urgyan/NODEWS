6964ab9007a8eb16d38577651a634f83e897e022
urgyan <urgyanue195125ece@gmail.com>
2021-11-17 18:57:54 +0530

    add controller router create post

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ router.get('/',homeController.home);
 
 router.use('/users',require('./users'));
 
+router.use('/posts', require('./posts'));
+
 // for any further routes access from here 
 // router.use('/routername', require('./routerfile));
 module.exports = router;
