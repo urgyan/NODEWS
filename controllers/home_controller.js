83384ed759e17bb778dc0a44bd01ae1a6beb2fc4
urgyan <urgyanue195125ece@gmail.com>
2021-11-17 20:19:59 +0530

    add controller to show username and content

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,23 @@
-module.exports.home = function(req,res){
-    return res.render('home',{
-        title:"Home"
-    });
-}
+// code to render home.ejs
+
+// module.exports.home = function(req,res){
+    // return res.render('home',{
+        // title:"Home"
+    // });
+// }
 
 // module.exports.action = function(req,res){ }
+
+const Post = require('../models/post');
+
+module.exports.home = function(req,res){
+
+    // to show user by populate the user of each post
+
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title:"Codeial | home",
+            posts: posts
+        });
+    });
+}
