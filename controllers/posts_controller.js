f03f809301872f779ef4b7a05f5966ee4507bdfd
urgyan <urgyanue195125ece@gmail.com>
2021-11-28 15:19:43 +0530

    convet ajax create post and delete post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,34 +1,70 @@
 const Post = require('../models/post')
 const Comment = require('../models/comment')
+const User = require('../models/user');
 
+// module.exports.create = async function(req, res){
 
-module.exports.create = async function(req, res){
-
-    try {
-        let post = await Post.create({
-            content: req.body.content,
-            user: req.user._id
+//     try {
+//         let post = await Post.create({
+//             content: req.body.content,
+//             user: req.user._id
         
-        });
+//         });
         
-        if(req.xhr){
-            return res.status(200).json({
-                data: {
-                    post: post
-                },
-                message:"Post Created!"
-            });
-        }
+//         if(req.xhr){
+//             return res.status(200).json({
+//                 data: {
+//                     post: post
+//                 },
+//                 message:"Post Created!"
+//             });
+//         }
 
-        req.flash('success','Post published !');
-        return res.redirect('back');
-    } catch (err) {
+//         req.flash('success','Post published !');
+//         return res.redirect('back');
+//     } catch (err) {
 
      
-        req.flash('error',err);
-        return res.redirect('back');
-    }
+//         req.flash('error',err);
+//         return res.redirect('back');
+//     }
    
+// }
+
+module.exports.create = async function(req,res)
+{
+    console.log(req.body);
+   let post = await Post.create({
+        content: req.body.content,
+        user: req.user._id                                                           // if error is something that canot read the property and you write the right code means syantatically correct then put it into the ''colon
+    })
+    
+      let posting =  await Post.findById(post.id)
+                    
+            .populate('user');
+
+           
+             
+          if(req.xhr){ 
+            return res.status(200).json({
+             data: {
+                 posting:posting
+             },
+             message: "post created!"
+            })
+        }
+      
+    
+        // end of the ajax request
+        if(err)
+        {
+            console.log('error at the posting the post',err);
+            return;
+        }
+        
+        res.redirect('back');
+  
+      
 }
 
 
@@ -45,7 +81,15 @@ module.exports.destroy = async function(req,res){
             post.remove();
 
            await Comment.deleteMany({post: req.params.id});
-
+           
+           if(req.xhr){
+               return res.status(200).json({
+                   data: {
+                       post_id:req.params.id
+                   },
+                   message:"Post deleted "
+               })
+           }
            req.flash('success','Post and associated comments deleted');
 
            return res.redirect('back');       
