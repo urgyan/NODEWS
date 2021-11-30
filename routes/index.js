470fd086fefe69aaa37d8004c2004431cd37f2a4
urgyan <urgyanue195125ece@gmail.com>
2021-11-30 16:46:54 +0530

    connect  main routes with api

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,9 @@ router.use('/posts', require('./posts'));
 
 router.use('/comments',require('./comments'));
 
+
+router.use('/api',require('./api'));
+
 // for any further routes access from here 
 // router.use('/routername', require('./routerfile));
 module.exports = router;
