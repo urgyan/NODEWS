6380175a50139277434c7a06102199e611d3f432
urgyan <urgyanue195125ece@gmail.com>
2021-12-02 20:55:38 +0530

    add a comment

--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -9,7 +9,7 @@ const postsApi = require("../../../controllers/api/v1/posts_api");
 router.get('/',postsApi.index);
 
 
-router.delete('/:id', passport.authenticate('jwt', {session: false}), postsApi.destroy);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), postsApi.destroy);  //session: false it prevent from generating session cookies
 
 
 module.exports = router;
