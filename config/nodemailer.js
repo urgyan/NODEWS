662a7ef8f4d90686b2ec440ed0cc3ab5152b719e
urgyan <urgyanue195125ece@gmail.com>
2021-12-04 18:55:57 +0530

    set up  nodemailer.j in config

---
+++ config/nodemailer.js
@@ -0,0 +1,45 @@
+const nodemailer = require('nodemailer');
+
+const ejs = require('ejs');
+const path = require('path');
+const { relative } = require('path');
+
+// this part defines how to communication take part 
+// htis part send the mail
+let transporter = nodemailer.createTransport({
+
+    service: 'gmail',
+
+    host: 'smtp.gmail.com',
+    port:587,
+    secure:false,
+
+    auth: {
+        user : 'urgyanue195125ece@gmail.com',
+        pass : 'urg#50475Y'
+    }
+
+});
+
+//  it defines the in mailers of views
+let renderTemplate = (data, relativePath) => {
+
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname, '../views/mailers', relativePath),
+        data,
+        function(err,template){
+
+            if(err){ console.log('error in rendering the template'); return;}
+
+            mailHTML = template;
+        }
+    )
+
+    return mailHTML;
+}
+
+module.exports = {
+    transporter: transporter,
+    renderTemplate: renderTemplate
+}
